020f39d1f38f1bdf19b4c8dd8d5ac095b49b3306
Dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2022-01-03 11:18:42 -0500

    app-bar

---
+++ src/components/app-bar/app-bar-container.jsx
@@ -0,0 +1,15 @@
+
+/**
+ * Contains business logic for App Bar
+ */
+export const AppBarContainer = () => {
+    return (
+        <>
+            <h1>Home</h1>
+            <h1>My Scans</h1>
+            <h1>Settings</h1>
+            <h1>Process Clip button</h1>
+            <h1>Bell</h1>
+        </>
+    )
+}
